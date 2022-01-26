03cc4d7a432be78ca07e015ab04bbaba2bcd1989
singhprajjwal4401 <98252433+singhprajjwal4401@users.noreply.github.com>
2022-01-26 10:42:30 +0530

    daily challenge

--- Assignment-Daily challenge/getMax.cpp
+++ Assignment-Daily challenge/getMax.cpp
@@ -1,40 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
 #include <stack>
-#include <sstream>
-#include <string>
 using namespace std;
 
+
 int main() {
-    
-    int n, query_type, query_value, maximum;
-    
-    string query_line;
-    stack<int> s, getMax;
+ 
+    stack<int> max;
+    stack<int> s;
+    max.push(0);
+    int n;
     cout<<"1 x -Push the element x into the stack."<<endl;
     cout<<"2 -Delete the element present at the top of the stack."<<endl;
     cout<<"3 -Print the maximum element in the stack."<<endl;
     cout<<"Enter the number of operations :"<<endl;
-    cin >> n;
-    cin.ignore();
-    for (int i = 0; i < n; i++) {
-        getline(cin, query_line);
-        istringstream ss(query_line);
-        ss >> query_type >> query_value;
-        switch (query_type) {
-        case 1:
-            s.push(query_value);
-            if (getMax.empty() || getMax.top() <= query_value)
-                getMax.push(query_value);
-            break;
-        case 2:
-            if (s.top() == getMax.top())
-                getMax.pop();
-            s.pop();
-            break;
-        default:
-            cout << getMax.top() << endl;
+    cin>>n;
+    while (n--){
+        int a;
+        cin>>a;
+        if(a==1){
+            cin>>a;
+            if(a>=max.top()) max.push(a);
+            s.push(a);
         }
+        else if(a==2){
+            if(s.top()==max.top()) max.pop();
+            s.pop();
+        } 
+        else if(a==3) cout<<max.top()<<endl;
     }
     return 0;
 }
